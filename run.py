ecb907875388b7da1ea82ecb7479b9954394a215
Hasnain Roopawalla <hasnain.roopawalla@gmail.com>
2019-09-29 13:55:26 +0530

    flask

--- run.py
+++ run.py
@@ -1,69 +1,23 @@
-from mrcnn.config import Config
-from mrcnn import model as modellib
-from mrcnn import visualize
-import numpy as np
-import colorsys
-import argparse
-import imutils
-import random
-import cv2
-import os
+from flask import Flask,flash,jsonify,request,render_template,redirect,url_for
+from PIL import Image
+import numpy
 
-import skimage.io
-class_names = ['BG', 'person', 'bicycle', 'car', 'motorcycle', 'airplane',
-               'bus', 'train', 'truck', 'boat', 'traffic light',
-               'fire hydrant', 'stop sign', 'parking meter', 'bench', 'bird',
-               'cat', 'dog', 'horse', 'sheep', 'cow', 'elephant', 'bear',
-               'zebra', 'giraffe', 'backpack', 'umbrella', 'handbag', 'tie',
-               'suitcase', 'frisbee', 'skis', 'snowboard', 'sports ball',
-               'kite', 'baseball bat', 'baseball glove', 'skateboard',
-               'surfboard', 'tennis racket', 'bottle', 'wine glass', 'cup',
-               'fork', 'knife', 'spoon', 'bowl', 'banana', 'apple',
-               'sandwich', 'orange', 'broccoli', 'carrot', 'hot dog', 'pizza',
-               'donut', 'cake', 'chair', 'couch', 'potted plant', 'bed',
-               'dining table', 'toilet', 'tv', 'laptop', 'mouse', 'remote',
-               'keyboard', 'cell phone', 'microwave', 'oven', 'toaster',
-               'sink', 'refrigerator', 'book', 'clock', 'vase', 'scissors',
-               'teddy bear', 'hair drier', 'toothbrush']
+app = Flask(__name__)
 
-def id_to_class(obj_ids):
-  obj_ids = dict((x,list(obj_ids).count(x)) for x in set(obj_ids))
-  for i in obj_ids:
-    print(obj_ids[i],' '+class_names[i])
-    
-ROOT_DIR = os.path.abspath("")
-MODEL_DIR = os.path.join(ROOT_DIR, "logs")
-COCO_MODEL_PATH = os.path.join(ROOT_DIR, "mask_rcnn_coco.h5")
+@app.route("/")
+def firstpage():
+    return render_template('index.html')
 
+@app.route("/getobjects",methods=["GET", "POST"])
+def newimage():
+    import detect
+    file = request.files['file']
+    img = Image.open(file).convert('RGB')
+    img.show() 
+    image1 = numpy.array(img) 
+    print(detect.getobj(image1))
+    return redirect('/')
 
-class SimpleConfig(Config):
-	# give the configuration a recognizable name
-	NAME = "coco_inference"
- 
-	# set the number of GPUs to use along with the number of images
-	# per GPU
-	GPU_COUNT = 1
-	IMAGES_PER_GPU = 1
- 
-	# number of classes (we would normally add +1 for the background
-	# but the background class is *already* included in the class
-	# names)
-	NUM_CLASSES = len(class_names)
-
-config = SimpleConfig()
-
-model = modellib.MaskRCNN(mode="inference", config=config,
-	model_dir=os.getcwd())
-model.load_weights(COCO_MODEL_PATH, by_name=True)
-
-
-image = skimage.io.imread('plane.jpg')
-
-# Run detection
-results = model.detect([image], verbose=1)
-id_to_class(results[0]['class_ids'])
-# Visualize results
-r = results[0]
-visualize.display_instances(image, r['rois'], r['masks'], r['class_ids'], 
-                            class_names, r['scores'])
-
+if __name__ == "__main__":
+    #threading.Timer(1.25, lambda: webbrowser.open("http://127.0.0.1:9000")).start()
+    app.run(host= '0.0.0.0', port=5000, debug=True)
